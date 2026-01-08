5c03d666e8b1eb136145b1203234a7f76c3deba2
PaulFarah7 <paul.f@echovalley.io>
2026-01-08 16:09:32 +0200

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -326,7 +326,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "43e2792e4c30456d63ef018ae51295e0
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "bb8179cfe063b97b03c1117d159122fd",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "5056e6d187cd20652bc3de761001d3fc",
+"flutter_bootstrap.js": "35bf17e4311e917cfd0ca7a3430371b9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",

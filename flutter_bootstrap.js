38a727fe153e45c6e2b355564a5ec2b381e1b9c3
MichelN10 <122791837+MichelN10@users.noreply.github.com>
2026-01-20 14:15:48 +0200

    dev

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1670997439"
+    serviceWorkerVersion: "3826542844"
   }
 });
